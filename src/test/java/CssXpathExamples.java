91055ae5c8b96da4285f4de599316ef988e9d6c6
Andrey <hanom_dzr@mail.ru>
2023-03-02 11:43:53 +0300

    added HW-done

---
+++ src/test/java/CssXpathExamples.java
@@ -0,0 +1,10 @@
+import static com.codeborne.selenide.Selenide.$;
+
+public class CssXpathExamples {
+    void cssXpathExamples() {
+
+
+        $("").setValue("1");
+
+    }
+}
--- src/test/java/TestBpxTests.java
+++ src/test/java/TestBpxTests.java
@@ -6,10 +6,11 @@ import org.junit.jupiter.api.Test;
 import java.nio.channels.ConnectionPendingException;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class TestBoxTests {
+public class TestBpxTests {
     @BeforeAll
     static void beforeAll() {
 
@@ -19,13 +20,26 @@ public class TestBoxTests {
 
     @Test
     void fillFormTest() {
-        open("/text-box");
-        $("#userName").setValue("Vaia Pupkin");
+        open("/automation-practice-form");
+        $("[id=firstName]").setValue("Vaia");
+        $("[id=lastName]").setValue("Petrov");
         $("#userEmail").setValue("test@test.com");
-        $("#currentAddress").setValue("Some address 1");
-        $("#permanentAddress").setValue("Some address 1");
+        $("#gender-radio-1").sendKeys(" ");
+        $("#userNumber").setValue("790000000000");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-dropdown-container").$(byText("May")).click();
+        $(".react-datepicker__year-select").$(byText("1990")).click();
+        $(".react-datepicker__month").$(byText("2")).click();
+        $("#subjectsInput").setValue("Mat").pressEnter();
+        $("#hobbies-checkbox-1").sendKeys(" ");
+        $("#hobbies-checkbox-2").sendKeys(" ");
+        $("#uploadPicture").uploadFromClasspath("pictures/pic.png");
+        $("#currentAddress").setValue("Lenina");
+        $("#react-select-3-input").setValue("ncr").pressEnter();   //вместо клика лишнего
+        $("#react-select-4-input").setValue("noi").pressEnter();
         $("#submit").click();
-        $("#output").shouldHave(text("Vaia Pupkin"),text("Vaia Pupkin"), text("test@test.com"),
-                text("Some address 1"), text("Some address 1"));
+        $(".modal-body").shouldHave(text("Vaia"),text("Petrov"), text("test@test.com"),
+                text("Male"), text("7900000000"), text("02 May,1990"), text("Math"), text("Sports"),
+                text("Reading"), text("pic.png"), text("Lenina"), text("ncr"), text("Noida"));
     }
 }
