852c6ad53fe40253819accca9e8b0cbbd613eae3
Andrey <hanom_dzr@mail.ru>
2023-05-09 21:27:19 +0300

    add properties branch with params tests

--- src/test/java/tests/SetBasePageSettings.java
+++ src/test/java/tests/SetBasePageSettings.java
@@ -14,12 +14,11 @@ import java.util.Map;
 public class SetBasePageSettings {
     @BeforeAll
     static void beforeAll() {
-
-        Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.browserVersion = "100.0";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserSize = System.getProperty("browser_size", "1920x1080");
+        Configuration.browserVersion = System.getProperty("browser_version","100.0");
+        Configuration.remote = "https://user1:1234@" + System.getProperty("selenoid_url", "selenoid.autotests.cloud/wd/hub");
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
